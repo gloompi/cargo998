e9483cfb91b3f0c0b7bfae46d38ef8d834724125
gloompi <gloompi@gmail.com>
2018-11-05 23:27:26 +0800

    prepare to share

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Provider } from 'mobx-react'
+import { ParallaxProvider } from 'react-scroll-parallax'
+
+import stores from './stores'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
+import './styles/normalize.css'
 import './App.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-        </header>
-      </div>
+      <Router>
+        <Provider {...stores}>
+          <ParallaxProvider>
+            <div className='App'>
+              <Header />
+              <Switch>
+                <Route exact path='/' component={Main} />
+                <Route path='/about' render={() => <h1>About</h1>} />
+              </Switch>
+              <Footer />
+            </div>
+          </ParallaxProvider>
+        </Provider>
+      </Router>
     )
   }
 }
