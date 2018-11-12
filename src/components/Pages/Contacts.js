b954694633fa1a6719e5e3183131b2e37aff7d9d
gloompi <gloompi@gmail.com>
2018-11-12 15:21:58 +0800

    Store House is done

--- src/components/Pages/Contacts.js
+++ src/components/Pages/Contacts.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { compose, lifecycle } from 'recompose'
-import axios from 'axios'
 import MaterialIcon from 'material-icons-react'
 
 const Contacts = () => (
@@ -21,29 +19,23 @@ const Contacts = () => (
               <p><MaterialIcon icon='room' size={25}/> <strong>Адрес нашего офиса: </strong>Chouzhou North Road 533, 5th floor, Yiwu city, Zhejiang Province, China</p>
             </div>
           </div>
+          <div className='contacts__wrapper storehouse'>
+            <div className='storehouse__pic'>
+              <img src="/images/storehouse.jpg" alt="StoreHouse"/>
+            </div>
+            <div className='contacts'>
+              <p><h3>Склад</h3></p>
+              <p><MaterialIcon icon='call' size={25}/> <strong>Liu Long: </strong>18611537778 </p>
+              <p><MaterialIcon icon='chat' size={25}/> <strong>wechat: </strong>18611537778 </p>
+              <p><MaterialIcon icon='chat_bubble_outline' size={25}/> <strong>QQ: </strong>5357321 </p>
+              <p><MaterialIcon icon='email' size={25}/> <strong>Email: </strong>5357321@qq.com</p>
+              <p><MaterialIcon icon='room' size={25}/> <strong>Адрес нашего склада: </strong>Qinglian District 3 (near Liantang District 4) 11, 1st Floor, 998 Treasury</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   </main>
 )
 
-export default compose(
-  lifecycle({
-    async componentDidMount() {
-      await axios.get('https://api-maps.yandex.ru/2.1/?apikey=7210ee31-4905-4c55-af20-6f6c9c09e6a6&lang=ru_RU')
-      const ymaps = window.ymaps
-     
-      const init = () => { 
-        const myMap = new ymaps.Map('map', {
-            center: [29.30861579979043,120.07148847870334],
-            zoom: 10
-        })
-
-        const myPlacemark = new ymaps.Placemark([29.30861579979043,120.07148847870334])
-        myMap.behaviors.disable('scrollZoom')
-        myMap.geoObjects.add(myPlacemark)
-      }
-      ymaps.ready(init)
-    }
-  })
-)(Contacts)
+export default Contacts
